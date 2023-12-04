93751929bfb4ce1978c73b18125d808740cedcde
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2023-12-04 21:53:41 +0000

    Reading Keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,6 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -7,8 +9,10 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-
+    private int x = 10;
+    private int y = 10;
     private Screen screen;
+
     Game() {
         try {
             Terminal terminal = new
@@ -22,14 +26,37 @@ public class Game {
         }
     }
 
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+        }
+    }
+
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                [0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if ((key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') && (key.getKeyType() == KeyType.EOF)){
+                break;
+            }
+            processKey(key);
+        }
     }
 }
