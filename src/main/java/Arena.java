ecf98971d198af817ad569645a54ab193183553f
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2023-12-05 21:57:13 +0000

    Walls

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -5,6 +5,9 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arena {
     private int width;
     private int height;
@@ -12,11 +15,26 @@ public class Arena {
 
     Hero hero = new Hero(10, 10);
 
+    private List<Wall> walls;
+
     Arena(int width, int height) {
         this.width = width;
         this.height = height;
+        this.walls = createWalls();
     }
 
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
+    }
     public void processKey(KeyStroke key) {
         switch (key.getKeyType()) {
             case ArrowUp:
@@ -38,6 +56,8 @@ public class Arena {
         screen.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         screen.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
         hero.draw(screen);
+        for (Wall wall : walls)
+            wall.draw(screen);
     }
 
     public void moveHero(Position position) {
@@ -46,6 +66,13 @@ public class Arena {
     }
 
     private boolean canHeroMove(Position position) {
-        return position.getX() < width && position.getX() >= 0 && position.getY() < height && position.getY() >= 0;
+        boolean not_blocked_by_walls = true;
+        for (Wall wall : walls) {
+            if (position.getX() == wall.getPosition().getX() && position.getY() == wall.getPosition().getY()) {
+                not_blocked_by_walls = false;
+                break;
+            }
+        }
+        return not_blocked_by_walls && (position.getX() < width && position.getX() >= 0 && position.getY() < height && position.getY() >= 0);
     }
 }
---
+++ src/main/java/Wall.java
@@ -0,0 +1,25 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Wall {
+    private Position position;
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+    Wall(int x, int y) {
+        this.position = new Position(x, y);
+    }
+
+    public void draw(TextGraphics screen) {
+        screen.setCharacter(position.getX(), position.getY(), TextCharacter.fromCharacter('W')[0]);
+        screen.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+        screen.putString(new TerminalPosition(position.getX(), position.getY()), "W");
+    }
+}
